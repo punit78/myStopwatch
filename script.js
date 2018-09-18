06171b000b239f2d7cc8d9954fa375605d499fc8
Puneet sharma <punit.name@gmail.com>
2018-09-18 22:14:07 +0530

    stopwatch demo done.

---
+++ script.js
@@ -0,0 +1,50 @@
+
+         var interval,count=0;
+         var clock=0;
+
+         //handle start/stop event
+        function start()
+        {
+          count++;
+          if(count<=1)
+          {
+              interval=setInterval(function(){
+                clock=clock+.01;
+                document.getElementById('resp').innerHTML=clock.toFixed(2); 
+              },100);
+          }
+          else{
+              clearInterval(interval);
+              count=0;
+          }
+         }
+
+         //reset functionbality
+         function reset()
+         {
+            clearInterval(interval);
+            clock=0;
+            count=0;
+            document.getElementById('resp').innerHTML=0;
+            document.getElementById('pasttime').innerHTML="";
+         }
+         function record()
+         {
+             var lis=document.createElement("li");
+             var mylist=document.getElementById("pasttime");
+             lis.innerHTML=clock.toFixed(2);
+             mylist.appendChild(lis);
+         }
+          //record functibnality iusing keypress or record
+         document.onkeypress =handlekeypress;
+          
+         function handlekeypress(event)
+         {
+             var mykey=event.key
+             if(mykey==='s')
+             start();
+             else if(mykey==='t')
+             record()
+             else if(mykey==='r')
+             reset();
+        }
